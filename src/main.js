10b982f875c1f91e92579bc215d6a4783e74ba61
DEPIDSVY <depidsvy@nintendo.de>
2016-08-22 17:18:15 +0200

    - FIX all bugs - started working

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-console.log('Hello World!');
-//# sourceMappingURL=main.js.map
+/// <reference path="../typings/index.d.ts" />
+
+console.log('Hello World! 3');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+	ReactDOM.render(
+		<Counter />,
+		document.getElementById('mount')
+	);
+});
