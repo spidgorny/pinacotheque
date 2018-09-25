039abeba3aaa0a28ef75f750dc7e0136d17340a5
Slawa Pidgorny <spidgorny@gmail.com>
2018-09-25 23:38:05 +0200

    - add saving meta

--- class/ScanExif.php
+++ class/ScanExif.php
@@ -1,6 +1,7 @@
 <?php
 
 use Intervention\Image\Constraint;
+use Intervention\Image\Image;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use Intervention\Image\ImageManager;
@@ -19,45 +20,85 @@ class ScanExif {
 		'tif',
 	];
 
+	/**
+	 * @var string
+	 */
+	protected $prefix;
+
+	/**
+	 * @var string
+	 */
+	protected $prefixMerged;
+
 	public function __construct(FileSystem $fileSystem)
 	{
 		$this->fileSystem = $fileSystem;
+		/** @var Local $adapter */
+		$adapter = $this->fileSystem->getAdapter();
+		$this->prefix = $adapter->getPathPrefix();
+		$this->prefixMerged = strtr($this->prefix, '/\\:', '___');
 	}
 
 	function __invoke()
 	{
-		/** @var Local $adapter */
-		$adapter = $this->fileSystem->getAdapter();
-		$prefix = $adapter->getPathPrefix();
-		echo 'Scanning ', $prefix, PHP_EOL;
-
-		$manager = new ImageManager();
+		echo 'Scanning ', $this->prefix, PHP_EOL;
 		$files = $this->fileSystem->listContents('', true);
 		echo 'Analyzing...', PHP_EOL;
 
+		$manager = new ImageManager();
 		$percent = new Percent(sizeof($files));
 //		print_r($files);
 		foreach ($files as $file) {
-			echo $percent->get(), '%', TAB, $prefix, TAB, $file['path'], PHP_EOL;
+			echo $percent->get(), '%', TAB, $this->prefix, TAB, $file['path'], PHP_EOL;
 			$percent->inc();
-			$ext = pathinfo($prefix.$file['path'], PATHINFO_EXTENSION);
+			$ext = pathinfo($this->prefix.$file['path'], PATHINFO_EXTENSION);
 			if (!in_array($ext, $this->imageTypes)) {
 				continue;
 			}
 			$image = $manager
-				->make($prefix.$file['path'])
-				->resize(256, null, function (Constraint $constraint) {
-				$constraint->aspectRatio();
-			});
+				->make($this->prefix.$file['path']);
 
-			$prefixMerged = strtr($prefix, '/\\:', '___');
-			$destination = __DIR__.'/../data/thumbs/'.$prefixMerged.'/'.$file['path'];
-			if (file_exists($destination)) {
-				continue;
-			}
-			@mkdir(dirname($destination), 0777, true);
-			$image->save($destination);
+			$this->saveMeta($image, $file);
+			$this->saveThumbnail($image, $file);
 		}
+	}
+
+	public function getDestinationFor($suffix)
+	{
+		$destination = __DIR__.'/../data/thumbs/'.$this->prefixMerged.'/'.$suffix;
+		return $destination;
+	}
+
+	public function saveMeta(Image $image, array $file)
+	{
+		$dirName = dirname($file['path']);
+		$jsonFile = $this->getDestinationFor($dirName.'/meta.json');
+		if (file_exists($jsonFile)) {
+			$fileContent = file_get_contents($jsonFile);
+			$json = json_decode($fileContent);
+		} else {
+			$json = (object)[];
+		}
+		$baseName = basename($file['path']);
+		if (isset($json->$baseName)) {
+			return;
+		}
+		$meta = $image->exif();
+		$json->$baseName = $meta;
+		file_put_contents($jsonFile, json_encode($json), JSON_PRETTY_PRINT);
+	}
+
+	public function saveThumbnail(Image $image, array $file)
+	{
+		$destination = $this->getDestinationFor($file['path']);
+		if (file_exists($destination)) {
+			return;
+		}
+		@mkdir(dirname($destination), 0777, true);
+		$image->resize(256, null, function (Constraint $constraint) {
+			$constraint->aspectRatio();
+		});
+		$image->save($destination);
 	}
 
 }
